ff6afdc5774d9eaa593920cfeab123281c194828
James Kelly <jamierkelly@btinternet.com>
2015-10-21 22:08:18 +0100

    Create Playgame.java

---
+++ Playgame.java
@@ -0,0 +1,10 @@
+public class Playgame{
+  
+   public static void main(String[] args)
+     {    
+          WorldChamp finals = new WorldChamp();
+          finals.init();
+          finals.match();
+     }
+  
+}
